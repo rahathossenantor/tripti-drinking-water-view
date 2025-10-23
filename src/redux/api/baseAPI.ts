18eb3ae7025518641b44453aba8bc9f2c52c3e70
rahathossenantor <md.rahathossenantor@gmail.com>
2025-10-24 00:42:52 +0600

    Backend url removed

--- src/redux/api/baseAPI.ts
+++ src/redux/api/baseAPI.ts
@@ -3,13 +3,9 @@ import { createApi } from "@reduxjs/toolkit/query/react";
 
 const baseApi = createApi({
     reducerPath: "api",
-    // baseQuery: axiosBaseQuery({ baseUrl: "http://localhost:5000/api/v1" }),
-    baseQuery: axiosBaseQuery({ baseUrl: "https://tripti-drinking-water.vercel.app/api/v1" }),
+    baseQuery: axiosBaseQuery({ baseUrl: "http://localhost:5000/api/v1" }),
     endpoints: () => ({}),
-    tagTypes: [
-        "customers",
-        "orders",
-    ],
+    tagTypes: ["customers", "orders"],
 });
 
 export default baseApi;
